82d6d7960598c9a65b1eb4651de4fdf19da71dcb
mishaambiliprakash <ab224105@gmail.com>
2020-12-22 22:41:33 +0530

    app completed

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Mathematical Opearations</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="add.php">Addition</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="sub.php">Subtraction</a>
+                
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="mult.php">Multiplication</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="div.php">Division</a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="images\math1.jpg" class="d-block w-100" alt="..." height="500" width="800">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="images\math3.jpg" class="d-block w-100" alt="..." height="500" width="800">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="images\maths2.jpg" class="d-block w-100" alt="..." height="500" width="800">
+                      </div>
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> 
+</body>
+</html>
---
+++ sub.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Mathematical Opearations</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="add.php">Addition</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="sub.php">Subtraction</a>
+                
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="mult.php">Multiplication</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="div.php">Division</a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-5 col-lg-6 col-xl-6 col-xxl-6">
+                <table class="table table-borderless">
+                    <tr>
+                        <td>Enter Num1</td>
+                        <td><input id="numa" type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Enter Num2</td>
+                        <td><input id="numb" type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button onclick="myButtonClick()" class="btn btn-primary">SUBTRACT</button></td>
+                    </tr>
+                    <tr>
+                        <td>result</td>
+                        <td><p id="result"></p></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    
+    <script>
+        function myButtonClick(){
+
+            var getnuma=parseInt(document.getElementById("numa").value)
+            var getnumb=parseInt(document.getElementById("numb").value)
+
+            var res=getnuma-getnumb
+            console.log(res)
+
+            document.getElementById("result").innerHTML=res
+
+            
+        }
+    </script>
+
+    }
+</body>
+</html>
